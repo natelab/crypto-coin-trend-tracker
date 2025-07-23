8b81aa0fef4266b679e959f341de1137c54404d7
Nathan <122789063+natelab@users.noreply.github.com>
2025-07-23 20:42:32 +0200

    Just need to connect to the api's now

--- Program.cs
+++ Program.cs
@@ -10,9 +10,20 @@ class Program
         Console.WriteLine("Enter cryptocurrency name (eg. Bitcoin); ");
         string coinId = Console.ReadLine();
 
-        Console.WriteLine("Enter the date (dd-mm-yyyy)");
+        Console.WriteLine("Enter the date (dd-MM-yyyy)");
         string dateInput = Console.ReadLine();
 
+        //Now to parse the user's date string
+        //If the date string is not inputed correctly then it will show an error message and exit the program
 
+        if (!DateTime.TryParseExact(dateInput, "dd-MM-yyyy", null, System.Globalization.DateTimeStyles.None, out DateTime date))
+        {
+            Console.WriteLine("Invalid date format.");
+            return;
+        }
+
+        Console.WriteLine("\nThe Program is fetching data, please wait patiently...\n"); //Telling the user to wait
+
+        
     }
 }
