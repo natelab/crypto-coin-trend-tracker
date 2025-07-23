402ffc91e9a2b628984d70e3a8ddafcc8e7df28e
Nathan <122789063+natelab@users.noreply.github.com>
2025-07-23 17:55:44 +0200

    The basics

--- Program.cs
+++ Program.cs
@@ -1,1 +1,18 @@
 ﻿using System;
+using System.Net.Http; //To make API calls/HTTP requests
+using System.Threading.Tasks; //For async and await
+using Newtonsoft.Json.Linq;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        Console.WriteLine("Enter cryptocurrency name (eg. Bitcoin); ");
+        string coinId = Console.ReadLine();
+
+        Console.WriteLine("Enter the date (dd-mm-yyyy)");
+        string dateInput = Console.ReadLine();
+
+
+    }
+}
